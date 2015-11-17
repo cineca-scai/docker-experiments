60dc71be13ba78c5007cf2b36045244cc53cc8da
Paulie D <p.donoriodemeo@gmail.com>
2015-11-17 17:34:33 +0100

    Fixing bugs and conceptual problems

--- images/irods_b2safe/install_eudat.sh
+++ images/irods_b2safe/install_eudat.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 #####################################
+yes $IRODS_PASS | sudo -S echo "Enabling priviledges"
+
+#####################################
 # Resources
 echo "Fix resources"
 iadmin rmresc demoResc
-# Create resources
+# Create resources dirs
+sudo chown -R $UID:$GROUPS $RESOURCES_DIR
+mkdir -p $MAINRES_DIR $REPLICA_DIR
+# Create resources
 iadmin mkresc $MAINRES unixfilesystem $IRODS_HOST:$MAINRES_DIR
 iadmin mkresc $REPLICARES unixfilesystem $IRODS_HOST:$REPLICA_DIR
 # Set new default
 sed -i "4s/demoResc/$MAINRES/" ~/.irods/irods_environment.json
 sed -i "15s/demoResc/$MAINRES/" /etc/irods/server_config.json
+
+# TO DO 
 
 ## source:http://irods.org/post/configuring-irods-for-high-availability/#.VktEE98veGg
 # iadmin mkresc BaseResource replication
@@ -20,20 +28,31 @@ sed -i "15s/demoResc/$MAINRES/" /etc/irods/server_config.json
 # ilsresc --tree
 
 #####################################
-# B2SAFE SERVICE
-cd /opt/eudat/b2safe/packaging  
+## B2SAFE SERVICE
+
+# Install package
+yes $IRODS_PASS | sudo -S dpkg -i /tmp/irods*.deb
+echo "Install successful. Ready to configure."
+
 # Configure
+cd /opt/eudat/b2safe/packaging  
 source $HANDLECONF
 keytochange='CHANGEME'
-sed -i "14s/$keytochange/$MAINRES"
-sed -i "19s/$keytochange/$HOSTNAME"
-sed -i "22s/$keytochange/$HANDLE_BASE"
-sed -i "23s/$keytochange/$HANDLE_USER"
-sed -i "24s/$keytochange/$HANDLE_PREFIX"
-mv myconf install.conf
+sudo chown $UID $EUDAT_CONFIGF
+sed -i "14s/$keytochange/$MAINRES/" $EUDAT_CONFIGF
+sed -i "19s/$keytochange/$HOSTNAME/" $EUDAT_CONFIGF
+sed -i "22s/$keytochange/$HANDLE_BASE/" $EUDAT_CONFIGF
+sed -i "23s/$keytochange/$HANDLE_USER/" $EUDAT_CONFIGF
+sed -i "24s/$keytochange/$HANDLE_PREFIX/" $EUDAT_CONFIGF
+cp $EUDAT_CONFIGF install.conf
 
-# Install b2safe
+# Install b2safe mod
 PASSFILE='mypass'
 echo $HANDLE_PASS > $PASSFILE
 ./install.sh < $PASSFILE
 rm $PASSFILE
+
+# Cleanup
+echo "Cleaning"
+sudo rm -rf /tmp/*
+echo "EUDAT setup completed"
